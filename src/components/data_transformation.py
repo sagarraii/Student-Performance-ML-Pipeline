75fb071c197900c2e2e6887fa9dfde2a22d1f459
SAGA dev <sagarrai9547@gmail.com>
2026-03-03 14:55:15 +0530

    UPDATED: Data Transformation file updated

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -121,7 +121,7 @@ class DataTransformation:
 
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
                 obj=preprocessing_obj
-
+                
             )
 
             return (
